ab4a657ecb9aead87b5636a75616740dcdb49aaf
Emerson-Requina <ercontreras@up.edu.ph>
2026-03-09 09:45:42 +0800

    Default Flutter App

---
+++ super_tic_tac_toe_midterms/README.md
@@ -0,0 +1,3 @@
+# super_tic_tac_toe_midterms
+
+A new Flutter project.
---
+++ super_tic_tac_toe_midterms/android/app/src/main/kotlin/com/example/super_tic_tac_toe_midterms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.super_tic_tac_toe_midterms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
